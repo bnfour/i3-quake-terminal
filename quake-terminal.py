ec11c380a90fbac62203856799b0edb6bae6392b
Bn4 <bnfour@bnfour.net>
2025-08-16 19:24:50 +0700

    pretty much converted the rest of the script: - it even works - there are no typing errors with standard type checking

--- quake-terminal.py
+++ quake-terminal.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
-# a script for i3 to have one global terminal available on hotkey
+# a script for i3 to have one global terminal available on hotkey,
+# now with typing!!!1
+
 # requires python3-i3ipc package
 
-# see https://github.com/bnfour/i3-quake-terminal for details
+# see https://github.com/bnfour/i3-quake-terminal for details,
+# MIT license
 
 #region imports
 
@@ -14,6 +17,7 @@ import time
 
 from enum import Enum
 from dataclasses import dataclass
+from operator import attrgetter
 from typing import cast, Final, NamedTuple, Self
 
 try:
@@ -204,9 +208,9 @@ def get_args() -> tuple[TypedConfig, list[str]]:
     height_group.add_argument('--relative-height', '-rh', type=float, dest='height_ratio',
         help='set the terminal window height relative to the output height')
 
-    parser.add_argument('--horizontal', '-x', choices=HorizontalAlignment.allowed(), default=defaults.horizontal_anchor,
+    parser.add_argument('--horizontal', '-x', choices=HorizontalAlignment.allowed(), default=defaults.horizontal_anchor.name.lower(),
         help='set the terminal window\'s horizontal align')
-    parser.add_argument('--vertical', '-y', choices=VerticalAlignment.allowed(), default=defaults.vertical_anchor,
+    parser.add_argument('--vertical', '-y', choices=VerticalAlignment.allowed(), default=defaults.vertical_anchor.name.lower(),
         help='set the terminal window\'s vertical align')
 
     parser.add_argument('--offset-horizontal', '-oh', '-ox', type=int, dest='offset_x', default=defaults.extra_offset.x,
@@ -298,35 +302,35 @@ def toggle(window: i3ipc.Con, i3: i3ipc.Connection, config: TypedConfig):
         else:
             hide(window)
 
-def show(window: i3ipc.Con, i3: i3ipc.Connection, config: argparse.Namespace):
+def show(window: i3ipc.Con, i3: i3ipc.Connection, config: TypedConfig):
     """
     Calls internal methods required to calculate the terminal window position
     and size and show it there.
     """
-    output_position, output_size = get_output_properties(config.output, i3)
-    output_width, output_height = output_size
-    # if relative ratio is set, it overrides size in pixels
-    width = int(config.width_ratio * output_width) if config.width_ratio else config.width
-    height = int(config.height_ratio * output_height) if config.height_ratio else config.height
+    output_region = get_output_properties(config.output, i3)
 
-    window_size = (width, height)
-    window_offset = (config.offset_x, config.offset_y)
+    match config.size:
+        case Size():
+            window_size = config.size
+        case SizeMultiplier():
+            window_size = output_region.size * config.size
     
-    window_position = get_position(output_position, output_size, window_size,
-        window_offset, config.horizontal, config.vertical)
+    window_position = get_position(output_region, window_size, config.extra_offset,
+        config.horizontal_anchor, config.vertical_anchor)
 
-    show_internal(window, window_position, window_size)
+    window_region = Region(window_position, window_size)
 
-def show_internal(window: i3ipc.Con, position: tuple[int, int], size: tuple[int, int]):
+    show_internal(window, window_region)
+
+def show_internal(window: i3ipc.Con, placement: Region):
     """
     Actually moves and resizes the window to dimensions specified.
     Sticky mode is applied only if not already set.
     """
-    x, y = position
-    w, h = size
-    sticky_command = '' if window.sticky else 'sticky enable,'
+    sticky_command = '' if window.sticky else 'sticky enable, ' # type: ignore
     window.command(f'scratchpad show, {sticky_command}'
-        + f' resize set {w}px {h}px, move position {x}px {y}px')
+        + f'resize set {placement.size.width}px {placement.size.height}px, '
+        + f'move position {placement.position.x}px {placement.position.y}px')
 
 def focus(window: i3ipc.Con):
     """Focuses the window"""
@@ -336,50 +340,48 @@ def hide(window: i3ipc.Con):
     """Hides the window by moving it to scratchpad"""
     window.command('move scratchpad')
 
-def get_output_properties(name: str, i3: i3ipc.Connection) -> tuple[tuple[int, int], tuple[int, int]]:
+def get_output_properties(name: str, i3: i3ipc.Connection) -> Region:
     """
-    Gets dimensions of a physical output by given name, or special 'main' value
-    that specifies the primary output, whatever its actual name is.
-    Returns output's dimesions as ((x position, y position), (width, height))
+    Gets screen position and size of a physical output by given name,
+    or special 'main' value that specifies the primary output, whatever its actual name is.
     """
     outputs = i3.get_outputs()
 
     if name == 'main':
-        filtered = tuple(out for out in outputs if out.primary)
+        filtered = tuple(out for out in outputs if out.primary) # type: ignore
     else:
-        filtered = tuple(out for out in outputs if out.name == name)
+        filtered = tuple(out for out in outputs if out.name == name) # type: ignore
     # surely there is no way two outputs will ever have the same name
     if len(filtered) != 1:
         print(f'Unable to find output "{name}".')
         sys.exit(1)
     
-    rect = filtered[0].rect
-    return ((rect.x, rect.y), (rect.width, rect.height))
+    rect = filtered[0].rect # type: ignore
 
-def get_position(output_origin: tuple[int, int], output_size: tuple[int, int],
-        window_size: tuple[int, int], window_offset: tuple[int, int],
-        h_anchor: str, v_anchor: str) -> tuple[int, int]:
+    return Region(Position(rect.x, rect.y), Size(rect.width, rect.height))
+
+def get_position(output: Region, window_size: Size, window_offset: Offset,
+        h_anchor: HorizontalAlignment, v_anchor: VerticalAlignment) -> Position:
     """Calculates the position for the terminal window per configuration provided"""
-    x0, y0 = output_origin
-    w0, h0 = output_size
-    w, h = window_size
-    ox, oy = window_offset
 
-    if h_anchor.startswith('l'):
-        x = x0
-    elif h_anchor.startswith('c'):
-        x = x0 + (w0 - w) // 2
-    elif h_anchor.startswith('r'):
-        x = x0 + w0 - w
+    match h_anchor:
+        case HorizontalAlignment.Left:
+            x = output.position.x
+        case HorizontalAlignment.Centre:
+            x = output.position.x + (output.size.width - window_size.width) // 2
+        case HorizontalAlignment.Right:
+            x = output.position.x + output.size.width - window_size.width
+    
+    match v_anchor:
+        case VerticalAlignment.Top:
+            y = output.position.y
+        case VerticalAlignment.Centre:
+            y = output.position.y + (output.size.height - window_size.height) // 2
+        case VerticalAlignment.Bottom:
+            y = output.position.y + output.size.height - window_size.height
+    
+    return Position(x, y) + window_offset
 
-    if v_anchor.startswith('t'):
-        y = y0
-    elif v_anchor.startswith('c'):
-        y = y0 + (h0 - h) // 2
-    elif v_anchor.startswith('b'):
-        y = y0 + h0 - h
-
-    return (x + ox, y + oy)
 
 def in_scratchpad(window: i3ipc.Con) -> bool:
     """Determines whether the provided window is off-screen in scratchpad"""
